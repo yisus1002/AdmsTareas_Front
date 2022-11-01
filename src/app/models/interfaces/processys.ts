9dacc95803f007d7a74c14b7804bd0af8f10737e
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-31 23:27:22 -0500

    Se creo una interfaz para los porcessys

---
+++ src/app/models/interfaces/processys.ts
@@ -0,0 +1,36 @@
+export class Processys{
+    nombreDeImagen:string;
+    PID: string;
+    nombreDeSesion:string;
+    numDeSesion: string;
+    usoDeMemoria: string;
+    estado:  string;
+    nombreDeUsuario:  string;
+    tiempoDeCpu:  string;
+    tituloDeVentana:  string;
+    quantum: number;
+
+    constructor(
+        nombreDeImagen:string,
+        PID: string,
+        nombreDeSesion:string,
+        numDeSesion: string,
+        usoDeMemoria: string,
+        estado:  string,
+        nombreDeUsuario:  string,
+        tiempoDeCpu:  string,
+        tituloDeVentana:  string,
+        quantum: number,
+    ){
+        this.nombreDeImagen = nombreDeImagen;
+        this.PID = PID;
+        this.nombreDeSesion = nombreDeSesion;
+        this.numDeSesion = numDeSesion;
+        this.usoDeMemoria = usoDeMemoria;
+        this.estado = estado;
+        this.nombreDeUsuario = nombreDeUsuario;
+        this.tiempoDeCpu = tiempoDeCpu;
+        this.tituloDeVentana = tituloDeVentana;
+        this.quantum = quantum;
+    }
+}
