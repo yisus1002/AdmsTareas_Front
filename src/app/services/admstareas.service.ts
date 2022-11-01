6c668d3be74cc703fbf57f43853991d17cc9e2d1
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-31 23:25:34 -0500

    Se creo un servicio para controlar la data de la tabla

--- src/app/services/admstareas.service.ts
+++ src/app/services/admstareas.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdmstareasService {
+  public api:string=`http://localhost:3000/`;
+  constructor(
+    private http:HttpClient
+  ) { }
 
-  constructor() { }
+  getProcessys():Observable<any>{
+    return (this.http.get<any>(`${this.api}processys`));
+  }
+  postProcessys():Observable<any>{
+    return (this.http.post<any>(`${this.api}processys`,''));
+  }
+  
+  
+  getProcess():Observable<any>{
+    return (this.http.get<any>(`${this.api}process`));
+  }
+  getProcessById(id:number):Observable<any>{
+    return (this.http.get<any>(`${this.api}process/${id}`));
+  }
+  delProcess(id:number):Observable<any>{
+    return (this.http.delete<any>(`${this.api}process/${id}`));
+  }
+
+  
+  
+  getCatalogue():Observable<any>{
+    return (this.http.get<any>(`${this.api}catalogue`));
+  }
+  postCatalogue(name:string):Observable<any>{
+    return (this.http.post<any>(`${this.api}catalogue`,{name}));
+  }
+
+  delCatalogue(id:number):Observable<any>{
+    return (this.http.delete<any>(`${this.api}catalogue/${id}`));
+  }
+
+
+
+
 }
---
+++ src/app/services/controlers.service.ts
@@ -0,0 +1,74 @@
+import { AdmstareasService } from 'src/app/services/admstareas.service';
+import { Injectable,  AfterViewInit } from '@angular/core';
+import { finalize } from 'rxjs';
+import { Processys } from '../models/interfaces/processys';
+import { MatTableDataSource } from '@angular/material/table'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControlersService implements AfterViewInit {
+
+  public   datos: Processys[]=[];
+  public options:number[]=[5,10,25, 100];
+  public loading:boolean=false; 
+
+  dataSource= new MatTableDataSource<Processys>();
+  
+  
+  constructor(private _sAdms: AdmstareasService) {
+    // this.listProcess();
+   }
+  ngAfterViewInit(): void {
+    
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+  }
+
+  listProcess(){
+    this.loading=false;
+    this._sAdms.getProcessys()
+    .pipe(finalize(()=>{
+      this.loading=true;
+    }))
+    .subscribe({
+      next: ((data:any)=>{ 
+        this.datos=data?.body?.response;
+        console.log(data);
+        this.dataSource.data=this.datos; 
+        this.options=[...this.options,(data?.body?.count)] 
+        console.log(this.dataSource)
+      }),
+      error: ((error:any)=>{
+        console.log(error);
+      })
+    })
+  }
+  ObtenerProccess(){
+    this.loading=false;
+    this._sAdms.postProcessys()
+    .pipe(finalize(()=>{
+      this.listProcess();
+    }))
+    .subscribe({
+      next: ((data:any)=>{
+        console.log(data)
+      }),
+      error: ((error:any)=>{
+        console.log(error);
+      })
+    })
+  }
+
+  crateCatalogue(name:string){
+    this._sAdms.postCatalogue(name)
+    .subscribe({
+      next: ((data:any)=>{
+        console.log(data)
+      }),
+      error:((error:any)=>{
+        console.log(error)
+      })
+    })
+  }
+}
